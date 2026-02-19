0ecd7199b1c5232f17b6eb9009a96f31b8b2662a
alepm03 <alejandropm03@gmail.com>
2026-02-19 09:56:07 +0100

    Fix base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/EdraiSolutions/',   // 👈 MUY IMPORTANTE para GitHub Pages
+    base: '/',   // 👈 MUY IMPORTANTE para GitHub Pages
     server: {
       port: 3000,
       host: '0.0.0.0',
